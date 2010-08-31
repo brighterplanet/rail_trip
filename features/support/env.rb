e5a249d16089c5f642592d2e514a7cbe6a5a27ec
Andy Rossmeissl <andy@rossmeissl.net>
2010-08-31 16:03:15 -0500

    Need this according to Derek

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,4 +5,4 @@ require 'cucumber'
 require 'cucumber/formatter/unicode'
 
 require 'sniff'
-Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => :rail
+Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => :rail, :cucumber => true
