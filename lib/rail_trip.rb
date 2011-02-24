6023ff3346098da67fef608c506637c7d621c5bb
Ian Hough <ian@brighterplanet.com>
2011-02-24 10:00:18 -0800

    Tweak scope description

--- lib/rail_trip.rb
+++ lib/rail_trip.rb
@@ -3,6 +3,6 @@ require 'emitter'
 module BrighterPlanet
   module RailTrip
     extend BrighterPlanet::Emitter
-    scope 'The rail trip emission estimate is the anthropogenic emissions per passenger from rail fuel combustion. It includes includes CO2 emissions from direct fuel combustion and fuel combustion to generate electricity.'
+    scope 'The rail trip emission estimate is the anthropogenic emissions per passenger from rail fuel combustion. It includes includes CO2 emissions from direct fuel combustion and indirect fuel combustion to generate purchased electricity.'
   end
 end
