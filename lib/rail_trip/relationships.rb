464846ae14f0b744719958c554532c8e6f8b1f5e
Seamus Abshere <seamus@abshere.net>
2011-10-22 17:43:11 -0500

    foreign keys

--- lib/rail_trip/relationships.rb
+++ lib/rail_trip/relationships.rb
@@ -2,10 +2,10 @@ module BrighterPlanet
   module RailTrip
     module Relationships
       def self.included(target)
-        target.belongs_to :country
-        target.belongs_to :rail_company
-        target.belongs_to :rail_class
-        target.belongs_to :rail_traction
+        target.belongs_to :country, :foreign_key => 'country_iso_3166_code'
+        target.belongs_to :rail_company, :foreign_key => 'rail_company_name'
+        target.belongs_to :rail_class, :foreign_key => 'rail_class_name'
+        target.belongs_to :rail_traction, :foreign_key => 'rail_traction_name'
       end
     end
   end
